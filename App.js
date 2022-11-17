dd3244fef599ba18bbb31450e3e9b0f0f7413153
contact-nidhi2 <contact.nidhi2@gmail.com>
2022-11-17 10:25:21 +0530

    final chnages

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View, StyleSheet,TextInput,TouchableOpacity } from 'react-native';
+import {Header} from 'react-native-elements';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+
+export default class App extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      text:"",
+      displayText:"",
+    }
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+       <Header
+      backgroundColor={'#9c8210'}
+      centerComponent={{text:'Monkey Chunkey', style: {color:'#fff',fontSize:20}}}/>
+      <TextInput style={styles.inputBox}
+      onChangeText = {(text)=>this.setState({text:text})}
+      value={this.state.text}/>
+      <TouchableOpacity
+      style={styles.goButton}
+      onPress={()=>{this.setState({displayText:this.state.text})}}>
+      <Text>Go</Text></TouchableOpacity>
+      <Text>{this.state.displayText}</Text>
+      </View>
+      
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
+ container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#b8b8b8',
   },
+  inputBox:{
+    marginTop:200,
+    width:"80%",
+    borderWidth:4
+  },
+  goButton:{
+    backgroundColor:"blue",
+  }
 });
